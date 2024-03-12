3175165367ae4163f4695fe0bc9b1d314776aa7b
Matheus <matheuscontato96@hotmail.com>
2024-03-12 09:07:00 -0300

    adicionando a estrutura do projeto

---
+++ database/migrations/2024_03_12_003051_create_livros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('descricao');
+            $table->unsignedBigInteger('userId');
+            $table->enum('estado_de_conservacao',['Ruim','Regular','Bom','Otimo']);
+            $table->enum('genero',["Ficção Científica", "Fantasia", "Romance", "Suspense", "Mistério", "Terror", "Não Ficção", "Biografia", "Autoajuda", "História", "Poesia", "Aventura"]);
+            $table->foreign('userId')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
